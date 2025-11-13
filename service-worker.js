715e7c51e2d085825d48e8e9fe4a7141555ba6ee
craversdjteam-a11y <craversdjteam@gmail.com>
2025-11-13 11:17:39 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,30 @@
-const CACHE = 'cravers-hub-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+const CACHE = 'cravers-hub-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/viewer.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
 });
+
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
